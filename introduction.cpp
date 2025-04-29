19b52f13e83721a67ab8e99f30bb1fd13aa2cf55
(faizsltn) <(faizsulthondaudm@gmail.com)>
2025-04-29 10:52:13 +0700

    membuat object dan menjalankan fungsi

--- introduction.cpp
+++ introduction.cpp
@@ -7,14 +7,18 @@ int nim;
 string nama;
 float nilai;
 
-void print_date(){
+void print_data(){
     cout << "NIM = " << nim << endl;
     cout << "NAMA = " << nama << endl;
     cout << "Nilai = " << nilai << endl;
     } 
-
 };
 
 int main(){
+    mahasiswa mhs;
+    mhs.nim = 2022;
+    mhs.nama = "Faizsltn";
+    mhs.nilai = 99;
   
+    mhs.print_data();
 }
